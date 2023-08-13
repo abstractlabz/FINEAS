63d10b0e4ecc391fe4eb6eead089fce6921199fa
kobbyidun <plasmadeveloper@outlook.com>
2023-08-13 16:26:51 -0400

    established the data ingestor

--- app/llm/chatbotdataingestor.py
+++ app/llm/chatbotdataingestor.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, jsonify
+from datasets import load_dataset
+from tqdm.auto import tqdm  # this is our progress bar
+import pinecone
 import openai
 import os
 from dotenv import load_dotenv
@@ -8,10 +11,12 @@ import hashlib
 app = Flask(__name__)
 
 load_dotenv()
-OPEN_AI_API_KEY = os.environ.get("OPEN_AI_API_KEY")
 PASS_KEY = os.environ.get("PASS_KEY")
+OPEN_AI_API_KEY = os.environ.get("OPEN_AI_API_KEY")
 openai.api_key = OPEN_AI_API_KEY
 MODEL = "text-embedding-ada-002"
+PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
+
 @app.route('/ingestor', methods=['POST'])
 def ingest_data():
     #get params
@@ -39,6 +44,38 @@ def ingest_data():
     )
     embeds = [record['embedding'] for record in res['data']]
     print(embeds)
+
+    #connection to pinecone
+    pinecone.init(
+    api_key=PINECONE_API_KEY,
+    environment="gcp-starter"
+    )
+
+    # check if 'openai' index already exists (only create index if not)
+    if 'devenv' not in pinecone.list_indexes():
+        pinecone.create_index('devenv', dimension=len(embeds[0]))
+    # connect to index
+    index = pinecone.Index('devenv')
+
+    # load the first 1K rows of the TREC dataset
+    trec = load_dataset('trec', split='train[:1000]')
+
+    batch_size = 32  # process everything in batches of 32
+    for i in tqdm(range(0, len(trec['text']), batch_size)):
+        # set end position of batch
+        i_end = min(i+batch_size, len(trec['text']))
+        # get batch of lines and IDs
+        lines_batch = trec['text'][i: i+batch_size]
+        ids_batch = [str(n) for n in range(i, i_end)]
+        # create embeddings
+        res = openai.Embedding.create(input=lines_batch, engine=MODEL)
+        embeds = [record['embedding'] for record in res['data']]
+        # prep metadata and upsert batch
+        meta = [{'text': line} for line in lines_batch]
+        to_upsert = zip(ids_batch, embeds, meta)
+        # upsert to Pinecone
+        index.upsert(vectors=list(to_upsert))
+
     return "200 Status OK"
 
 if __name__ == '__main__':
