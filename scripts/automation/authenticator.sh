4f0c1c8d7f329bc635f920a167b50dcbf35fb0fe
0xPCDefenders <abstractlabz@proton.me>
2024-07-22 01:14:14 -0400

    cleaning automation scripts

--- scripts/automation/authenticator.sh
+++ scripts/automation/authenticator.sh
@@ -1,14 +1,15 @@
-#TO RESET SSL CERTIFICATES
+#!/bin/bash
+
+# Load sensitive keys from JSON file
+CONFIG_FILE="keys.json" #or populate keys_template.json with your keys and rename it to keys.json
+CLOUDFLARE_EMAIL=$(jq -r '.CLOUDFLARE_EMAIL' $CONFIG_FILE)
+CLOUDFLARE_API_KEY=$(jq -r '.CLOUDFLARE_API_KEY' $CONFIG_FILE)
+ZONE_ID=$(jq -r '.ZONE_ID' $CONFIG_FILE)
 
 # Variables
 DOMAIN="${CERTBOT_DOMAIN}"
 TOKEN="${CERTBOT_VALIDATION}"
 RECORD="_acme-challenge.${DOMAIN}"
-
-# Cloudflare API credentials
-CLOUDFLARE_EMAIL="kobenaidun@gmail.com"
-CLOUDFLARE_API_KEY="083c5ca93134656f451044d76e44ea2a4172c"
-ZONE_ID="88f7983cb0dc455e60bf1ca2fd43161f"
 
 # Add TXT record to Cloudflare
 curl -s -X POST "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records" \
